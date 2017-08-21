e69d425ab8d90a3f8d90cf2b7af466ffbb9abff2
liuzhan <liuzhan@mindai.com>
2017-08-21 10:02:57 +0800

    学习

--- test01.py
+++ test01.py
@@ -1,10 +1,19 @@
 import numpy as np
 
 my0 = np.zeros([3, 5])
-print my0
+# print my0
 
 my1 = np.ones([2, 3])
-print my1
+# print my1
 
 myRandom = np.random.rand(2, 2)
-print myRandom
+# print myRandom
+
+myeye = np.eye(3)
+# print myeye
+
+myones = np.ones([3, 3])
+# print myeye + myones
+# print myones - myeye
+
+
