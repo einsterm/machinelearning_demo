b4c61a699491d93dd0b34d1a32cfcdb15737a744
einsterm <519044887@qq.com>
2017-08-31 12:53:21 +0800

    高斯

--- text/t03.py
+++ text/t03.py
@@ -3,7 +3,6 @@ import sys
 import os
 import jieba
 import cPickle as pk
-from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.datasets.base import Bunch
 from sklearn.naive_bayes import MultinomialNB
@@ -12,54 +11,19 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 
+# 写入文件
 def saveFile(savePath, content):
     f = open(savePath, "wb")
     f.write(content)
     f.close()
 
 
+# 读取文件
 def readFile(filePath):
     f = open(filePath, "rb")
     content = f.read()
     f.close()
     return content
-
-
-def fenci(type):
-    path1 = "F:/python_wks/text_clustering_small/" + type + "/no/"
-    path2 = "F:/python_wks/text_clustering_small/" + type + "/yes/"
-    fileList = os.listdir(path1)
-    for dirName in fileList:
-        fileDir = path1 + dirName
-        saveDir = path2 + dirName
-        if not os.path.exists(saveDir):
-            os.makedirs(saveDir)
-        fileList = os.listdir(fileDir)
-        for fileName in fileList:
-            filePath = fileDir + "/" + fileName
-            savePath = saveDir + "/" + fileName
-            content = readFile(filePath)
-            seg = jieba.cut(content)
-            saveFile(savePath, " ".join(seg))
-
-
-def bunchOp(type):
-    fenciPath = "F:/python_wks/text_clustering_small/" + type + "/yes/"
-    bunchPath = "F:/python_wks/text_clustering_small/" + type + "/bunch/" + type + "_set.dat"
-    bunch = Bunch(target_name=[], label=[], filenames=[], contents=[])
-    categoryList = os.listdir(fenciPath)  # 分词后文件夹里所有的内容
-    bunch.target_name.extend(categoryList)
-    for categoryDirName in categoryList:
-        categoryDir = fenciPath + categoryDirName
-        fileList = os.listdir(categoryDir)
-        for fileName in fileList:
-            filePath = categoryDir + "/" + fileName
-            bunch.label.append(categoryDirName)
-            bunch.filenames.append(filePath)
-            bunch.contents.append(readFile(filePath).strip())
-    bunchFileObj = open(bunchPath, "wb")
-    pk.dump(bunch, bunchFileObj)
-    bunchFileObj.close()
 
 
 def readBunch(filePath):
@@ -75,26 +39,67 @@ def writeBunch(savePath, bunchObj):
     f.close()
 
 
+# 分语,type是指要放在那个文件夹，有训练数据和测试数据
+def fenci(type):
+    path1 = "F:/python_wks/text_clustering_small/" + type + "/no/"  # 分词前
+    path2 = "F:/python_wks/text_clustering_small/" + type + "/yes/"  # 分词后
+    fileList = os.listdir(path1)
+    for dirName in fileList:
+        fileDir = path1 + dirName
+        saveDir = path2 + dirName
+        if not os.path.exists(saveDir):
+            os.makedirs(saveDir)
+        fileList = os.listdir(fileDir)
+        for fileName in fileList:
+            filePath = fileDir + "/" + fileName
+            savePath = saveDir + "/" + fileName
+            content = readFile(filePath)
+            # 下面是jieba分词
+            seg = jieba.cut(content)
+            saveFile(savePath, " ".join(seg))
+
+
+# 生成bunch数据结构文件，方便生成向量空间模型
+def bunchOp(type):
+    fenciPath = "F:/python_wks/text_clustering_small/" + type + "/yes/"
+    bunchPath = "F:/python_wks/text_clustering_small/" + type + "/bunch/" + type + "_set.dat"
+    bunch = Bunch(target_name=[], label=[], filenames=[], contents=[])
+    categoryList = os.listdir(fenciPath)  # 分词后文件夹里所有的内容
+    bunch.target_name.extend(categoryList)  # 所有类别名称列表
+    for categoryDirName in categoryList:
+        categoryDir = fenciPath + categoryDirName
+        fileList = os.listdir(categoryDir)
+        for fileName in fileList:
+            filePath = categoryDir + "/" + fileName
+            bunch.label.append(categoryDirName)  # 类别
+            bunch.filenames.append(filePath)  # 文件路径
+            bunch.contents.append(readFile(filePath).strip())  # 文件内容
+    bunchFileObj = open(bunchPath, "wb")
+    pk.dump(bunch, bunchFileObj)
+    bunchFileObj.close()
+
+
+# 生成训练向量词袋
 def trainSpaceOp():
     stopWordsPath = "F:/python_wks/text_clustering_small/train/stopWords/stop.txt"
     bunchObjPath = "F:/python_wks/text_clustering_small/train/bunch/train_set.dat"
-    spacePath = "F:/python_wks/text_clustering_small/train/bunch/tfidfspace.dat"
+    spacePath = "F:/python_wks/text_clustering_small/train/bunch/trainSpace.dat"
     stopWordList = readFile(stopWordsPath).splitlines()  # 停用词列表
     trainBunch = readBunch(bunchObjPath)
     trainSpace = Bunch(target_name=trainBunch.target_name, label=trainBunch.label, filenames=trainBunch.filenames,
                        tdm=[],
                        vocabulary={})
     vectorizer = TfidfVectorizer(stop_words=stopWordList, sublinear_tf=True, max_df=0.5)
-    transformer = TfidfTransformer()
-    trainSpace.tdm = vectorizer.fit_transform(trainBunch.contents)
+    trainSpace.tdm = vectorizer.fit_transform(trainBunch.contents)  # 转换为词频矩阵
     trainSpace.vocaluary = vectorizer.vocabulary_
     writeBunch(spacePath, trainSpace)
 
 
+# 生成测试向量词袋
 def testSpaceOp():
     stopWordsPath = "F:/python_wks/text_clustering_small/test/stopWords/stop.txt"
     testBunchObjPath = "F:/python_wks/text_clustering_small/test/bunch/test_set.dat"
-    trainSpacePath = "F:/python_wks/text_clustering_small/train/bunch/tfidfspace.dat"
+    trainSpacePath = "F:/python_wks/text_clustering_small/train/bunch/trainSpace.dat"
     testSpacePath = "F:/python_wks/text_clustering_small/test/bunch/testSpace.dat"
     stopWordsList = readFile(stopWordsPath).splitlines()
 
@@ -107,7 +112,6 @@ def testSpaceOp():
 
     vectorizer = TfidfVectorizer(stop_words=stopWordsList, sublinear_tf=True, max_df=0.5,
                                  vocabulary=trainBunch.vocaluary)
-    transformer = TfidfTransformer()
 
     testSpace.tdm = vectorizer.fit_transform(testBunch.contents)
     testSpace.vocaluary = trainBunch.vocaluary
@@ -115,27 +119,24 @@ def testSpaceOp():
     writeBunch(testSpacePath, testSpace)
 
 
+# 预测分类
 def yuche():
-    trainBunchPath = "F:/python_wks/text_clustering_small/train/bunch/tfidfspace.dat"
+    trainBunchPath = "F:/python_wks/text_clustering_small/train/bunch/trainSpace.dat"
     testSpacePath = "F:/python_wks/text_clustering_small/test/bunch/testSpace.dat"
-    trainBunch = readBunch(trainBunchPath)
-    testSpace = readBunch(testSpacePath)
-    clf = MultinomialNB(alpha=0.001).fit(trainBunch.tdm, trainBunch.label)
-    predicted = clf.predict(testSpace.tdm)
-    total = len(predicted)
-    rate = 0
+    trainBunch = readBunch(trainBunchPath)  # 训练
+    testSpace = readBunch(testSpacePath)  # 测试
+    clf = MultinomialNB(alpha=0.001).fit(trainBunch.tdm, trainBunch.label)  # 朴素贝叶斯算法
+    predicted = clf.predict(testSpace.tdm)  # 预测分类结果
     for flabel, file_name, expct_cate in zip(testSpace.label, testSpace.filenames, predicted):
-        if flabel != expct_cate:
-            rate += 1
-            print file_name
-            print flabel
-            print expct_cate
+        print file_name  # 文件名
+        print flabel  # 实际类别
+        print expct_cate  # 预测类别
 
 
 if __name__ == '__main__':
     # fenci("train")
     # bunchOp("train")
-    #  trainSpaceOp()
+    # trainSpaceOp()
     # fenci("test")
     # bunchOp("test")
     # testSpaceOp()
