a49f1023a8b3a340b21c74c7f6307c46ee0e7076
einsterm <519044887@qq.com>
2017-09-07 15:10:07 +0800

    决策树算法

--- demo04/tree.py
+++ demo04/tree.py
@@ -3,6 +3,7 @@
 import math
 import operator
 import matplotlib.pyplot as plt
+import pickle
 
 
 # 计算熵
@@ -108,21 +109,12 @@ def plotNode(nodeTxt, centerPt, parentPt, nodeType):
                             va="center", ha="center", bbox=nodeType, arrowprops=arrow_args)
 
 
-def createPlot():
-    fig = plt.figure(1, facecolor='white')
-    fig.clf()
-    createPlot.ax1 = plt.subplot(111, frameon=False)
-    plotNode(U'node1', (0.5, 0.1), (0.1, 0.5), descNode)
-    plotNode(U'node2', (0.8, 0.1), (0.3, 0.8), leafNode)
-    plt.show()
-
-
 def getNumLeafs(myTree):
     numLeafs = 0
     firstStr = myTree.keys()[0]
     secondDict = myTree[firstStr]
     for key in secondDict.keys():
-        if type(secondDict).__name__ == 'dict':
+        if type(secondDict[key]).__name__ == 'dict':
             numLeafs += getNumLeafs(secondDict[key])
         else:
             numLeafs += 1
@@ -134,13 +126,62 @@ def getTreeDepth(myTree):
     firstStr = myTree.keys()[0]
     secondDict = myTree[firstStr]
     for key in secondDict.keys():
-        if type(secondDict).__name__ == 'dict':
+        if type(secondDict[key]).__name__ == 'dict':
             thisDepth = 1 + getTreeDepth(secondDict[key])
         else:
             thisDepth = 1
         if thisDepth > maxDepth:
             maxDepth = thisDepth
     return maxDepth
+
+
+def plotMidText(cntrPt, parentPt, txtString):
+    xMid = (parentPt[0] - cntrPt[0]) / 2.0 + cntrPt[0]
+    yMid = (parentPt[1] - cntrPt[1]) / 2.0 + cntrPt[1]
+    createPlot.ax1.text(xMid, yMid, txtString)
+
+
+def plotTree(myTree, parentPt, nodeTxt):
+    numLeafs = getNumLeafs(myTree)
+    depth = getTreeDepth(myTree)
+    firstStr = myTree.keys()[0]
+    cntrPt = (plotTree.xOff + (1.0 + float(numLeafs)) / 2.0 / plotTree.totalW, plotTree.yOff)
+    plotMidText(cntrPt, parentPt, nodeTxt)
+    plotNode(firstStr, cntrPt, parentPt, descNode)
+    secondDict = myTree[firstStr]
+    plotTree.yOff = plotTree.yOff - 1.0 / plotTree.totalD
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            plotTree(secondDict[key], cntrPt, str(key))
+        else:
+            plotTree.xOff = plotTree.xOff + 1.0 / plotTree.totalW
+            plotNode(secondDict[key], (plotTree.xOff, plotTree.yOff), cntrPt, leafNode)
+            plotMidText((plotTree.xOff, plotTree.yOff), cntrPt, str(key))
+    plotTree.yOff = plotTree.yOff + 1.0 / plotTree.totalD
+
+
+def createPlot(inTree):
+    fig = plt.figure(1, facecolor='white')
+    fig.clf()
+    axprops = dict(xticks=[], yticks=[])
+    createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)
+    plotTree.totalW = float(getNumLeafs(inTree))
+    plotTree.totalD = float(getTreeDepth(inTree))
+    plotTree.xOff = -0.5 / plotTree.totalW;
+    plotTree.yOff = 1.0
+    plotTree(inTree, (0.5, 1.0), '')
+    plt.show()
+
+
+def saveTree(inTree, filename):
+    fw = open(filename, 'w')
+    pickle.dump(inTree, fw)
+    fw.close()
+
+
+def readTree(filename):
+    fr = open(filename)
+    return pickle.load(fr)
 
 
 if __name__ == "__main__":
@@ -154,4 +195,6 @@ if __name__ == "__main__":
     # print best
 
     # print majorityCnt(a)
-    createPlot()
+    inTree = createTree(dataSet, labels)
+    print inTree
+    createPlot(inTree)
