d2884b009774526e2cc397b4564343ff7fbc7932
einsterm <519044887@qq.com>
2017-10-17 16:33:44 +0800

    多元线性回归

--- demo06/test4.py
+++ demo06/test4.py
@@ -1,4 +1,7 @@
-import math
+# --*-- encoding:utf-8 --*--
+'''
+求导数为0时，自变量的值
+'''
 
 
 def f_prime(x_old):
@@ -6,12 +9,12 @@ def f_prime(x_old):
 
 
 def cal():
-    x_old = 0
-    x_new = 4
-    eps = 0.01
+    x_old = 0  # 导数为0时，x的初始值
+    x_new = 50  # 导数为0时，x的估计值
+    eps = 0.4
     presision = 0.00001
 
-    while abs(x_new - x_old) > presision:
+    while abs(x_new - x_old) > presision:  # abs 返回绝对值 上一次求导的值减去本次求导的值,如果趋势是0，则认为极值存在
         x_old = x_new
         x_new = x_old + eps * f_prime(x_old)
     return x_new
