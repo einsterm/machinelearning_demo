64b300a1a2c52791c5c1d3d30d5bbe8d2d275e2a
einsterm <519044887@qq.com>
2017-10-27 11:08:46 +0800

    SVM算法

--- demo07/test3.py
+++ demo07/test3.py
@@ -11,16 +11,17 @@ np.random.seed(0)
 X = np.array([[1, 1], [1, 0], [0, 2], [2, 2], [4, 3], [4, 1], [3, 4], [5, 4]])
 Y = [0] * 4 + [1] * 4
 
-
 clf = svm.SVC(kernel='linear')
 clf.fit(X, Y)
 
+# w_0*x+w_1*y+w_2=0
+# y=-(w_0/w_1)*x-(w_2/w_1) 这里就是点斜式
 w = clf.coef_[0]
 a = -w[0] / w[1]
 xx = np.linspace(-1, 5)
-yy = a * xx - (clf.intercept_[0]) / w[1]
+yy = a * xx - (clf.intercept_[0]) / w[1]  # clf.intercept_[0] 是截距
 
-b = clf.support_vectors_[0]
+b = clf.support_vectors_[0]  # 支持向量机的几个点
 yy_down = a * xx + (b[1] - a * b[0])
 b = clf.support_vectors_[-1]
 yy_up = a * xx + (b[1] - a * b[0])
