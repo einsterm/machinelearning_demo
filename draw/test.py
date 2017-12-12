053b53aa0f7db971ec5ede54cde01323c11e49bd
liuzhan <519044887@qq.com>
2017-12-12 21:39:08 +0800

    随机森林算法

--- draw/test.py
+++ draw/test.py
@@ -1,13 +1,11 @@
 import numpy as np
 
 a = np.mat(np.array([3, 3, 0, 2, 0, -2, 1, -2])).T
-b = np.array([1555, 25555555])
+b = np.array([1, 2])
+f = np.array([[3, 2,1],[1,1,1],[2,2,2]])
 d = np.mat(np.array([[2, 3], [1, 2], [3, 4]]))
 e = np.array([True, False])
 c = np.nonzero(a)
 # d = a[:, 0]
-print(d)
-print("--------")
-print(d[:-1])
-print("--------")
-print(np.var(d[:-1]))
+print(np.linalg.det(f))
+
