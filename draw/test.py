ca9e4a78f36265f47561f90ce787d938d7e62fb9
liuzhan <519044887@qq.com>
2017-12-19 20:53:10 +0800

    pca算法

--- draw/test.py
+++ draw/test.py
@@ -1,13 +1,21 @@
 import numpy as np
 
 a = np.mat(np.array([6, 3, 0, 2, 1, -2, 21, -2]))
-b = np.mat([[-3, 1], [1, -3]])
-f = np.mat([[0, 0, -2], [1, 2, 1], [1, 0, 3]])
+A = np.mat([[1, 0], [2, -3]])
 d = np.mat(np.array([[2, 1], [4, 3]]))
 dd = np.mat(np.array([[1, 1], [2, 2]])).T
 e = np.array([True, False])
-c = np.nonzero(a)
+f = np.mat([[0, 0, -2], [1, 2, 1], [1, 0, 3]])
+c = np.nonzero(f)
+# print(c)
+U = np.mat([[.16, .99], [.99, -.16]])
+S = np.mat([[3.65, 0], [0, .85]])
+V = np.mat([[.58, .81], [-.81, .58]])
+# print(np.linalg.svd(A))
 
-t,v=np.linalg.eig(f)
-print(t)
-print(v)
+# unratedItems = nonzero(dataMat[user, :].A == 0)[1]
+
+print(f[:, :2])
+# print(np.nonzero(f[2, :]))
+# -0.6464   	0.7630
+# 0.7630   	0.6464
