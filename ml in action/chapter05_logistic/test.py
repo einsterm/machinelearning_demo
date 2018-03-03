d08d938d534163f447920745c6a8891fe5e0d29c
albert <einsterm@github.com>
2018-03-03 21:40:53 +0800

    bayes

--- ml in action/chapter05_logistic/test.py
+++ ml in action/chapter05_logistic/test.py
@@ -19,22 +19,32 @@ def sigmoid(inX):
     return 1.0 / (1 + exp(-inX))
 
 
-def gradAscent(dataMatIn, classLabels):
-    dataMatrix = mat(dataMatIn)  # 转化为numpy矩阵
-    labelMat = mat(classLabels).transpose()  # 矩阵转置，行变成列，列变成行
-    rows, cloumns = shape(dataMatrix)  # 取矩阵的长度
+def gradAscent(dataSet, labels):
+    dataSetM = mat(dataSet)  # 转化为numpy矩阵
+    labelsM = mat(labels).transpose()  # 矩阵转置，行变成列，列变成行
+    rows, cloumns = shape(dataSetM)  # 取矩阵的长度
     alpha = 0.001
     maxCycles = 500
-    weightsMat = ones((cloumns, 1))  # 假设最优参数是1的矩阵
+    weights = ones((cloumns, 1))  # 假设最优参数是1的矩阵
     for k in range(maxCycles):
-        cloumnSumVal = dataMatrix * weightsMat  # 把每一行的属性相加起来，即:f(x)=w0*x0+w1*x1+w2*x2
-        hMat = sigmoid(cloumnSumVal)  # 求出二分值
-        errorMat = (labelMat - hMat)  # 误差
-        dataMatrixT = dataMatrix.transpose()  # 矩阵求导结果 dataMatrix*errorMat （矩阵的求导公式）
-        dataMatrix1 = alpha * dataMatrixT
-        dataMatrix2 = dataMatrix1 * errorMat  # 这个相当于损失函数
-        weightsMat = weightsMat + dataMatrix2
-    return weightsMat
+        y = dataSetM * weights  # 把每一行的属性相加起来，即:f(x)=w0*x0+w1*x1+w2*x2
+        hMat = sigmoid(y)  # 求出二分值
+        errorMat = (labelsM - hMat)  # 误差
+        weights = weights + alpha * dataSetM.transpose() * errorMat  # 根据公式推导出来
+    return weights
+
+
+def stocGradAscent(dataSet, labels):
+    dataSetM = array(dataSet)
+    m, n = shape(dataSetM)
+    alpha = 0.01
+    weights = ones(n)
+    for i in range(m):
+        oneLine = dataSetM[i]
+        h = sigmoid(sum(oneLine * weights))
+        error = labels[i] - h
+        weights = weights + alpha * error * oneLine
+    return weights
 
 
 def plotBestFit(weights):
@@ -66,8 +76,8 @@ def plotBestFit(weights):
 
 if __name__ == '__main__':
     dataMat, labelMat = loadDataSet()
-    weights = gradAscent(dataMat, labelMat)
-    plotBestFit(weights.getA())
+    weights = stocGradAscent(dataMat, labelMat)
+    plotBestFit(weights)
     # a = mat([[1, 2, 3], [1, 1, 1]])
     # b = mat([[4], [5], [6]])
     # print a * b
