d9f2f97be8195db042d97e8ace76932e3924b669
albert <einsterm@github.com>
2018-03-22 20:49:03 +0800

    cnn神经网络算法

--- draw/test.py
+++ draw/test.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
 # coding: utf-8
-from numpy import *
 from sklearn import preprocessing
+import numpy as np
 import matplotlib.pyplot as plt
 
-d = mat([[4, 2], [3, 6], [4, 2], [5, 2]])
-f = mat([[1, 2], [2, 13]])
-A = mat([[1, 0], [2, -3]])
 
-# print f.sum(axis=1)
-print(f[1,0])
+a = np.arange(0, 60, 10)
+for x in np.nditer(a):
+    print(x)  # 0 5 10 15 20 25 30 35 40 45 50 55
+
+# 修改数组的值: nditer对象的一个可选参数op_flags,其默认值为只读,但可以设置为读写或只写模式.这将允许使用此迭代器修改数组元素
+for x in np.nditer(a, op_flags=['readwrite']):
+    x[...] = 2 * x;
+    print(x)
