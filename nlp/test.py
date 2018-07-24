a6443b74f700fadfc787351f968c761b8842447b
albert <einsterm@github.com>
2018-07-24 22:24:36 +0800

    opencv

--- nlp/test.py
+++ nlp/test.py
@@ -4,15 +4,15 @@ from scipy import stats
 
 n = 3
 k = np.arange(n + 1)
-pcoin = stats.binom.pmf(k, n, float(2.0 / 3))
+pcoin = stats.binom.pmf(2, 3, 0.6)
 print(pcoin)
-x = stats.multinomial.pmf([1, 1, 1],n=3, p=[0.3, 0.2, 0.5])
+x = stats.multinomial.pmf([1, 0, 2],n=3, p=[0.3, 0.2, 0.5])
 print(x)
 
-x = stats.multinomial.pmf([1, 2, 1],n=3, p=[0.3, 0.2, 0.5])
-print(x)
+x = stats.multinomial.pmf([1, 1, 1],n=3, p=[0.3, 0.2, 0.5])
+# print(x)
 
 x = stats.multinomial.pmf([1, 2],n=3, p=[0.66666666666667, 0.33333333333333])
-print(x)
+# print(x)
 # pl.stem(k, pcoin, basefmt="k-")
 # pl.margins(0.1)
